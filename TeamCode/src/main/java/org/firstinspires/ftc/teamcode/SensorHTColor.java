df93dcddb15798413230e7b76d60dc8555959268
aarushsivanesan <1497800@fcpsschools.net>
2017-10-28 10:45:37 -0400

    Enabled for testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorHTColor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorHTColor.java
@@ -34,7 +34,6 @@ import android.graphics.Color;
 import android.view.View;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 
@@ -50,7 +49,6 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 @Autonomous(name = "Sensor: HT color", group = "Sensor")
-@Disabled
 public class SensorHTColor extends LinearOpMode {
 
   ColorSensor colorSensor;  // Hardware Device Object
